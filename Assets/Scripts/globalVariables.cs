ad81bd7e00566a32ac2f4edea9610fe2fa05e74c
Katie T <kttackett123@gmail.com>
2022-11-14 15:42:09 -0500

    imported globalVariables script; made scenes

---
+++ Assets/Scripts/globalVariables.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class globalVariables : MonoBehaviour
+{
+    // used to track the number of items the player has
+    public static int itemsCollected = 0;
+
+    // how much time the player has left
+    public static float timer = 0;
+}
